06fbad26741cf63e80d687da2b2f2d5b58315fc8
Solsospecial <solomonilogieneayo@gmail.com>
2025-08-20 23:23:28 +0100

    Update pdf_wrapper.py

--- part-2/pdf_wrapper.py
+++ part-2/pdf_wrapper.py
@@ -1,7 +1,7 @@
 from typing import Optional
 from langchain_core.callbacks.manager import CallbackManagerForToolRun
 from langchain_core.pydantic_v1 import BaseModel, Extra
-from langchain_core.tools import BaseTool
+from langchain_core.tools.base import BaseTool
 
 class PDFSearchAPIWrapper(BaseModel):
     """Wrapper for PDF Search API."""
