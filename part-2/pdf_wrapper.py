a8f8bbebc592282465b1e558360bf7146749bee5
Solsospecial <solomonilogieneayo@gmail.com>
2025-08-20 23:21:17 +0100

    Update pdf_wrapper.py

--- part-2/pdf_wrapper.py
+++ part-2/pdf_wrapper.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from langchain_core.callbacks import CallbackManagerForToolRun
+from langchain_core.callbacks.manager import CallbackManagerForToolRun
 from langchain_core.pydantic_v1 import BaseModel, Extra
 from langchain_core.tools import BaseTool
 
