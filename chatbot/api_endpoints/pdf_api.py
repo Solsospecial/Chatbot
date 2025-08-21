6187312cf6cea7ad98d1a37feef4b245863523ee
Solsospecial <solomonilogieneayo@gmail.com>
2025-08-21 20:44:51 +0100

    Update pdf_api.py

--- chatbot/api_endpoints/pdf_api.py
+++ chatbot/api_endpoints/pdf_api.py
@@ -1,12 +1,9 @@
 from fastapi import File, UploadFile, APIRouter
 from starlette.responses import JSONResponse
 from pydantic_models import QueryRequest
-from typing import List
 from chrome import add_linkedin_messages, model, messages_collection,client
 import os
 from pathlib import Path
-import chromadb
-import uuid
 from langchain_chroma import Chroma
 import logging
 
@@ -19,19 +16,20 @@ UPLOAD_DIRECTORY = Path("upload")
 os.makedirs(UPLOAD_DIRECTORY, exist_ok=True)  # Ensure the directory exists
 
 @router.post("/add_pdf/")
-async def upload_csv(file: UploadFile = File(...)):
+async def upload_pdf(file: UploadFile = File(...)):
     if not file.filename.endswith('.pdf'):
         return JSONResponse(status_code=400, content={"error": "Only PDF allowed"})
     file_path = UPLOAD_DIRECTORY / file.filename
     with open(file_path, "wb") as pdf_file:
         pdf_file.write(await file.read())
-        print("File saved successfully:", file_path)
+    logger.info(f"File saved successfully: {file_path}")
         
     try:
         add_linkedin_messages(pdf_file_path=file_path)
         return JSONResponse(status_code=200, content={"message": "LinkedIn messages uploaded and processed successfully."})
     except Exception as e:
-        return JSONResponse(status_code=500, content={"error": f"Failed to process CSV file: {str(e)}"})
+        logger.exception("Error while processing PDF")
+        return JSONResponse(status_code=500, content={"error": f"Failed to process the PDF file: {str(e)}"})
     
 @router.post("/search_query_in_pdf/")
 async def query_messages(request: QueryRequest):
@@ -58,4 +56,4 @@ async def query_messages(request: QueryRequest):
         extracted_data+=content
         extracted_data+=" "
     print(extracted_data)
-    return extracted_dat
+    return extracted_data
