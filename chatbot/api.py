4f5ef6ec3535c28187fb1596ba4bdcb8ee124402
Solsospecial <solomonilogieneayo@gmail.com>
2025-08-21 20:17:13 +0100

    Update api.py

--- chatbot/api.py
+++ chatbot/api.py
@@ -1,10 +1,9 @@
-from api_endpoints import pdf_api,web_api
+from api_endpoints import pdf_api, web_api
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from prometheus_fastapi_instrumentator import Instrumentator
 from starlette.middleware.sessions import SessionMiddleware
 import logging
-
 
 logging.basicConfig(filename='api.log', filemode='a', level=logging.INFO, \
                     format='%(asctime)s - %(name)s - %(levelname)s - %(funcName)s - %(lineno)d- %(message)s')
@@ -25,7 +24,3 @@ app.add_middleware(SessionMiddleware, secret_key="your-secret-key")
 
 app.include_router(pdf_api.router, tags=["PDF Question Answering"])
 app.include_router(web_api.router, tags=["Website Question Answering"])
-# app.include_router(bot_details.router, tags=["Bot Details"])
-# app.include_router(response.router, tags=["Get Answer"])
-# app.include_router(linkedin_api.router, tags=["Linkedin messages Operations"])
-
