546af07c631a0e13ec9018e9c46d1da299d9353b
Solsospecial <solomonilogieneayo@gmail.com>
2025-08-21 15:16:11 +0100

    Update pydantic_models.py

--- chatbot/pydantic_models.py
+++ chatbot/pydantic_models.py
@@ -2,9 +2,6 @@ from pydantic import BaseModel
 
 class QueryRequest(BaseModel):
     input: str
-
-class MessageResponse(BaseModel):
-    content: str
-
+    
 class WebDataRequest(BaseModel):
     url: str
