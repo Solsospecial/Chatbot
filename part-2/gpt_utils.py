c02a702b6e98a3dcdb6cfbf7352a9977e28b988e
Solsospecial <solomonilogieneayo@gmail.com>
2025-08-21 15:02:56 +0100

    Update gpt_utils.py

--- part-2/gpt_utils.py
+++ part-2/gpt_utils.py
@@ -1,32 +1,34 @@
 from pdf_wrapper import PDFSearchAPIWrapper
 from web_wrapper import WebSearchAPIWrapper
-from langchain_google_community.search import GoogleSearchAPIWrapper
-from langchain.tools.base import StructuredTool
+from langchain_google_community import GoogleSearchAPIWrapper
+from langchain.tools import StructuredTool
 
+# Configure Google Search tool
 def google_tool():
     google_search = GoogleSearchAPIWrapper()
     google_tool = StructuredTool.from_function(
+        func=google_search.run,
         name="google-search",
-        description="Search Google for recent results.",
-        func=google_search.run
+        description="Search Google for recent results."
     )
     return google_tool
 
+# Configure Web Search tool
 def web_tool():
     web_search = WebSearchAPIWrapper()
     web_tool = StructuredTool.from_function(
+        func=web_search.run,
         name="web-search",
-        description="Searches in URL data to get the relavent chunk based on the  user-query.",
-        func=web_search.run
+        description="Searches in URL data to get the relavent chunk based on the  user-query."
     )
     return web_tool
 
+# Configure PDF Search tool
 def pdf_tool():
     pdf_search = PDFSearchAPIWrapper()
     pdf_tool = StructuredTool.from_function(
+        func=pdf_search.run,
         name="pdf-search",
-        description="Searches in pdf to get the relavent chunk based on the  user-query. ",
-        func=pdf_search.run
+        description="Searches in pdf to get the relavent chunk based on the  user-query. "
     )
     return pdf_tool
-
