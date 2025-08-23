f8e709ee4443729c411d38cfb490843c5e3f1b97
Solsospecial <solomonilogieneayo@gmail.com>
2025-08-23 18:01:10 +0100

    Update app.py

--- chatbot/app.py
+++ chatbot/app.py
@@ -61,16 +61,15 @@ with st.sidebar:
             response = requests.post(f"http://127.0.0.1:8000/add_pdf/", files={"file": file_uploader})
             if response.status_code == 200:
                 st.success("✅ PDF document uploaded successfully")
+                if not allow_reupload:
+                    st.warning('INFO: Re-uploading the same PDF is disabled by default. Tick the checkbox "Allow re_upload" to enable re-upload')
+                else:
+                    st.warning('STATUS: ✅ PDF Re-upload is enabled')
                 st.session_state.pdfs.append(file_str)
             else:
                 st.error(f"Failed to upload PDF. Status code: {response.status_code}")
                 st.error("Response content: " + response.text)
-        else:
-            st.warning('This PDF has already been uploaded. Tick the checkbox "Allow re_upload" to enable re-upload')
-        
-        # Clear input after processing
-        st.session_state.file_input = None
-
+                
     if url:
         url_str = str(url.strip())
         if url_str not in st.session_state.urls or allow_reupload:
@@ -80,16 +79,15 @@ with st.sidebar:
                 response = requests.post(f"http://127.0.0.1:8000/scrape_webdata/", json={"url": url})
                 if response.status_code in (200, 202):
                     st.success("✅ Web Data Extracted")
+                    if not allow_reupload:
+                        st.warning('INFO: Re-entering the same URL is disabled by default. Tick the checkbox "Allow re_upload" to enable re-entry')
+                    else:
+                        st.warning('STATUS: ✅ URL Re-entery is enabled')
                     st.session_state.urls.append(url_str)
                 else:
                     st.error(f"Failed to extract web data. Status code: {response.status_code}")
                     st.error("Response content: " + response.text)
-        else:
-            st.warning('This URL has already been processed. Tick the checkbox "Allow re_upload" to enable re-upload')
-    
-        # Clear input after processing
-        st.session_state.url_input = ""
-        
+
 # Create the LangChain agent
 if "agent_executor" not in st.session_state:
     try:
